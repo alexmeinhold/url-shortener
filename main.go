7129a016724b35b56f23df157413ddad3cf43128
Alexander Meinhold <alexander.meinhold@outlook.com>
2020-09-23 20:54:14 +0200

    replace mux router with builtin functionality

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/syndtr/goleveldb/leveldb"
+	"log"
 	"math/rand"
 	"net/http"
 )
@@ -20,19 +20,17 @@ func RandStringBytes(n int) string {
 	return string(b)
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
+func handlePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	url := r.PostFormValue("url")
 
 	// TODO: check for collisions
 	key := RandStringBytes(8)
 
 	// TODO: check if url starts with http://
-	fmt.Printf("put %s into db\n", url)
 	err = db.Put([]byte(key), []byte(url), nil)
 
 	if err != nil {
@@ -43,16 +41,27 @@ func post(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fullUrl))
 }
 
-func redirect(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	// 1. get key from url
+	// TODO: check if key is in valid format
+	key := r.URL.Path[1:]
+	// 2. look up key in database to get url
 	newUrl, err := db.Get([]byte(key), nil)
-
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
+	// 3. send redirect response to user with url
 	http.Redirect(w, r, string(newUrl), http.StatusSeeOther)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s %s\n", r.Method, r.URL)
+
+	if r.Method == "POST" {
+		handlePost(w, r)
+	} else if r.Method == "GET" {
+		handleRedirect(w, r)
+	}
 }
 
 func main() {
@@ -63,10 +72,8 @@ func main() {
 		panic(err)
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", post).Methods("POST")
-	r.HandleFunc("/{id}", redirect)
-	http.ListenAndServe(":8080", r)
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":8080", nil)
 
 	defer db.Close()
 }
